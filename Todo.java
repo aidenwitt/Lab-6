913571649b5c60ac7881b18b64d95e4c67bcb409
aidenwitt <47369128+aidenwitt@users.noreply.github.com>
2019-02-21 14:31:11 -0600

    Add files via upload

---
+++ Todo.java
@@ -0,0 +1,51 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-21
+OU ID_______________: 113472850
+OU EMAIL____________: aidenwitt@ou.edu
+
+Tasks:
+1. Complete 'Circle' Class
+2. Complete 'Shape' Class
+3. Complete 'Polygon' Class
+4. Complete 'EquilateralTriangle' Class
+5. Complete 'Trapezoid' Class
+6. Complete 'Shape Sorter' Class
+7. Complete 'Rectangle' Class
+8. Complete 'Square' Class
+9. Complete 'ShapeAreaComparator' Class
+10. Complete 'ShapePerimeterComparator' Class
+11. Complete 'ShapeSorterTest'
+12. Complete 'ShapeTest'
+13. Run all test and ensure appropriate coverage on all files, modifying both test files if needed.
+
+Estimated Time (minutes):
+1. 5
+2. 5
+3. 5
+4. 5
+5. 5
+6. 5
+7. 10
+8. 5
+9. 10
+10. 10
+11. 20
+12. 20
+13. 10
+
+Actual Time (minutes):
+1. 5
+2. 4
+3. 5
+4. 3
+5. 2
+6. 4
+7. 8
+8. 1
+9. 5
+10. 5
+11. 15
+12. 15
+13. 25
+*/}
